44c8d6db3defce015d91739bc8d6f5e0c5779c9d
Fernando Lara <fmaia8@hotmail.com>
2023-05-23 15:39:35 -0300

    Verifica Valores

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
+import Swal from 'sweetalert2';
+
 const search = document.getElementById('search');
 const input = document.getElementById('input');
 const valores = document.getElementById('valores');
+
+const testValues = (base) => {
+  if (input.value === '') {
+    // Swal.fire('Ops...', 'Você precisa passar uma moeda!', 'error');
+    throw new Error('Você precisa passar uma moeda!');
+  }
+  if (input.value !== base) {
+    // Swal.fire('Ops...', 'Moeda não existente!', 'error');
+    throw new Error('Moeda não existente!');
+  }
+};
 
 const createTitle = (base) => {
   const newH2 = document.createElement('h3');
@@ -9,11 +22,11 @@ const createTitle = (base) => {
 };
 
 const listElements = (obj) => {
+  testValues(obj.base);
   valores.innerHTML = '';
   createTitle(obj.base);
   const objList = Object.entries(obj.rates);
   objList.forEach((objts) => {
-    console.log(objts);
     const newDiv = document.createElement('div');
     const newH3 = document.createElement('h3');
     const newP = document.createElement('p');
@@ -27,5 +40,8 @@ const listElements = (obj) => {
 search.addEventListener('click', () => {
   fetch(`https://api.exchangerate.host/latest?base=${input.value}`)
     .then((response) => response.json())
-    .then((response) => listElements(response));
+    .then((response) => listElements(response))
+    .catch((error) => {
+      Swal.fire('OPS...', `${error.message}`, 'error');
+    });
 });
